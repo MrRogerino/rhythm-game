932f92d8f22aa3ede24360df73d5d74f6c3e4e5b
Roger <weihsuan.li24@gmail.com>
2018-11-29 19:18:46 -0700

    create skybox script and collider

---
+++ Assets/Scripts/Camera Effects/SkyboxChangeScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxChangeScript : MonoBehaviour {
+
+    public Material skyboxMat;
+
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.gameObject.CompareTag("NoteBar")){
+            RenderSettings.skybox = skyboxMat;
+        }
+    }
+}
